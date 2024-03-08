60c7bbce1b5f2b3652a4244e149eb101751c92ca
Ahmed Samir Ahmed <ahmed.samirelbakrey@gmail.com>
2024-03-08 09:58:32 +0200

    (feature/addingBuildSrc) -> Update (Android block -> buildConfig) Android project configuration in build.gradle.kts
    - Update compileSdk, minSdk, targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,36 +16,36 @@ android {
 
     /**
      * compileSdk -> API level is used by Gradle to compile your app(usually the same as targetSdk)
-     *
+     *-----------------------------------------------------------------
      * (More Details about compileSdk)
-     *
+     *-----------------------------------------------------------------
      * If your app uses an API that is introduced in API level 26, your compileSdk must be set to minimum 26. If you set it to 25, it fails to compile.
-     *
      * Assuming the minSdk is 21, you likely get this warning/error too
      *
-     * Call requires API level 26 (current min is 21): android.app.NotificationChannel()
+     * -----> Call requires API level 26 (current min is 21): android.app.NotificationChannel() <-----
      *
      * As recommended by the Android Studio IDE, you can fix the warning/error by annotating your function with
-     *
+     *-----------------------------------------------------------------
      * @RequiresApi(Build.VERSION_CODES.O)
      * private fun yourFunction() {
      *     /*...*/
      * }
+     *-----------------------------------------------------------------
      *
      * However, if you run your App on the Android version (< API level 26), it will NOT crash but fails silently. You will notice the error messages in the logcat. So, I think adding the @RequiresApi() is a bad practice here.
-     *
      * What you should do is handle the code above the app runs below the API level 26.
      *
+     *-----------------------------------------------------------------
      * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
      *     /* Code for API level >= 26 */
      * } else {
      *    /* code for API level >= 23 and < 26) */
      * }
-     *
+     *-----------------------------------------------------------------
      *
      * An example is the notification channel is only required / available for API level 26. Then, we should wrap it with Build.VERSION_CODES.O.
      *
-     *
+     *-----------------------------------------------------------------
      *if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
      *
      *     val notificationChannel = NotificationChannel(
@@ -59,7 +59,16 @@ android {
      *
      *     notificationManager?.createNotificationChannel(notificationChannel)
      * }
+     *-----------------------------------------------------------------
+     * Conclusion
+     *-----------------------------------------------------------------
+     * In general,
+     * minSdk < targetSdk <= compileSdk
      *
+     * but ideally and practically (100% good work),
+     * minSdk < targetSdk == compileSdk == latest SDK version
+     *
+     * Don't use @RequiresApi() and handle the different API versions' behavior in the code.
      */
     compileSdk = ProjectConfiguration.compileSdk
 
@@ -76,8 +85,9 @@ android {
 
         /**
          * minSdk -> Minimum API Level required for the app to run
-         *
+         *-----------------------------------------------------------------
          * (More Details about minSdk)
+         *-----------------------------------------------------------------
          * If your minSdk is set to 21, your app cannot be run on any Android version that is below API level 21. If the Android version (API level 20) attempts to install your app, you get this error.
          *
          * Installation did not succeed. The application could not be installed: INSTALL_FAILED_OLDER_SDK
@@ -88,8 +98,9 @@ android {
 
         /**
          * targetSdk -> API level the app is designed and tested on (usually the same as compileSdk)
-         *
+         *-----------------------------------------------------------------
          * (More Details about targetSdk)
+         * -----------------------------------------------------------------
          * (**App runs on API level > targetSdk**)
          * If the app is run on the Android version (API level) that is higher than the targetSdk, the Android operating system will try to run backward compatibility behavior to match behavior as in targetSdk API level.
          *
@@ -98,11 +109,25 @@ android {
          * If your targetSdk is set to 22 and your app runs on Android version (API level 23), the Android OS will try to match the behavior as in API level 22. Thus, no runtime permission is requested.
          *
          * If your targetSdk is set to 23 and your app runs on Android version (API level 23 or higher), runtime permission is requested.
-         *
+         *-----------------------------------------------------------------
          * (**App runs on API level < targetSdk**)
          * What about the app that runs on the Android version (API level) that is < targetSdk? The app behaves based on that Android version (API level).
          *
          * If your targetSdk is set to 23 and your app runs on Android version (API level 22), runtime permission is not requested.
+         *
+         * ------------------------------------------------------------------------------------------------------------------------------------------------------------
+         *                runtime app permission is introduced in API level 23 or higher ||| Before API level 23, runtime app permission is not needed.
+         * ------------------------------------------------------------------------------------------------------------------------------------------------------------
+         *                                                          In case Of (App runs on API level > targetSdk)
+         *                                                          ----------------------------------------------
+         * target SDK  22    ||||   your app runs on Android version (API level 23)             ----> no runtime permission is requested. (the android OS will try to match the behavior as in the Api level 22)
+         *
+         * target SDK  23    ||||   your app runs on Android version (API level 23 or higher)   ----> runtime permission is requested. (the android OS will try to match the behavior as in the Api level 22)
+         * ------------------------------------------------------------------------------------------------------------------------------------------------------------
+         *                                                          In case Of (App runs on API level < targetSdk)
+         *                                                          ----------------------------------------------
+         * target SDK  23    ||||   your app runs on Android version (API level 22)             ----> runtime permission is not requested.
+         *
          */
         targetSdk = ProjectConfiguration.targetSdk
 
